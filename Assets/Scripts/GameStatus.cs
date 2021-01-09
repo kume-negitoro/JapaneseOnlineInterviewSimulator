c351ba960ac69db0706f8ba0a364dd3ac5604fdf
kume-negitoro <kume.negitoro@gmail.com>
2021-01-09 11:17:39 +0900

    add GameStatus

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameStatus
+{
+    static string playerName = "山田太郎";
+    static int playerAge = 21;
+    static string companyName = "株式会社一般";
+    static int score = 0;
+}
