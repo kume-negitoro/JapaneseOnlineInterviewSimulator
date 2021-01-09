31dd6418a1415181bfc6c5befd3915f5ff048b5e
kume-negitoro <kume.negitoro@gmail.com>
2021-01-09 11:54:18 +0900

    add SceneManager

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public static class GameStatus
 {
-    static string playerName = "山田太郎";
-    static int playerAge = 21;
-    static string companyName = "株式会社一般";
-    static int score = 0;
+    //名前
+    public static string playerName = "山田太郎";
+    //年齢
+    public static int playerAge = 21;
+    //会社名
+    public static string companyName = "株式会社一般";
+    //スコア
+    public static int score = 0;
+    //長所
+    public static string pros = "";
+    //短所
+    public static string cons = "";
 }
---
+++ Assets/Scripts/TitmeSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitmeSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("あなたの名前は" + GameStatus.playerName + "です");
+        Debug.Log("あなたの年齢は" + GameStatus.playerAge + "です");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
